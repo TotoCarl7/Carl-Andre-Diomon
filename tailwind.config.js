b8cc70d3974aba091f25f1534649931e42f25308
Carl Andre R. Diomon <cardiomon@antiquespride.edu.ph>
2024-09-23 01:42:13 +0800

    Final 1.0.01

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     screens:{
       sm: "340px",
       md: "540px",
       lg: "1020px",
-      xl: "1280px"
+      xl: "1440px"
     },
     extend: {},
   },
